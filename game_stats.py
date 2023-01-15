8a0d5a6148210a09afdb5460f92c10b9010b6e08
alexprog <khazanchuk@yandex.ru>
2023-01-15 22:08:38 +0000

    15/01/2023

---
+++ game_stats.py
@@ -0,0 +1,8 @@
+class GameStats:
+    def __init__(self, ai_game):
+        self.settings = ai_game.settings
+        self.reset_status()
+        self.game_active = True
+
+    def reset_status(self):
+        self.ships_left = self.settings.ship_limit
